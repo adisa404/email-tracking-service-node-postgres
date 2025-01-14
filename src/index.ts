8203bbeb035cfcc584c9ae93644efdb952c865ce
Adisa Mrvoljak <adisamrvoljak@gmail.com>
2025-01-15 00:21:24 +0100

    add test get request. add prettier file.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import routes from './routes/tracking.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.use('/api/tracking', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
